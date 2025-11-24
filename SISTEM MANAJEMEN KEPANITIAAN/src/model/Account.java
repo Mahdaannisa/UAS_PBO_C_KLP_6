5175144cf375f9852cbc0506d088ab817b0e44d5
SilviaPutri <silviaptrrii@gmail.com>
2025-11-24 21:41:15 +0700

    Add abstract Account class with authentication methods
    Added abstract class to represent an account with basic information and authentication methods.

--- SISTEM MANAJEMEN KEPANITIAAN/src/model/Account.java
+++ SISTEM MANAJEMEN KEPANITIAAN/src/model/Account.java
@@ -1,1 +1,7 @@
+package model;
 
+/**
+ * Kelas abstrak yang merepresentasikan sebuah akun dalam sistem.
+ * Kelas ini menyimpan informasi dasar seperti username, password, dan nama,
+ * serta menyediakan metode untuk melakukan pengecekan autentikasi.
+ */
