d74aafaead82316e759d21a7cf966a8f9e6959b0
arshaalifamahmud <arshaalifamahmud@gmail.com>
2025-11-24 11:09:19 +0700

    Divisi.java

--- SISTEM MANAJEMEN KEPANITIAAN/src/model/Divisi.java
+++ SISTEM MANAJEMEN KEPANITIAAN/src/model/Divisi.java
@@ -21,3 +21,31 @@ public class Divisi {
      * @param nama nama divisi
      */
     public Divisi(String nama) { this.nama = nama; }
+
+    /**
+     * Mengambil nama divisi.
+     *
+     * @return nama divisi
+     */
+    public String getNama() { return nama; }
+
+    /**
+     * Mengambil daftar tugas yang dimiliki divisi ini.
+     *
+     * @return list tugas dalam divisi
+     */
+    public List<Tugas> getTugasList() { return tugasList; }
+
+    /**
+     * Menambahkan sebuah tugas ke dalam divisi.
+     *
+     * @param t tugas yang akan ditambahkan
+     */
+    public void addTugas(Tugas t) { if (t!=null) tugasList.add(t); }
+
+    /**
+     * Menghapus tugas berdasarkan ID tugas.
+     *
+     * @param id ID tugas yang akan dihapus
+     */
+    public void removeTugasById(String id) { tugasList.removeIf(t->t.getId().equals(id)); }
