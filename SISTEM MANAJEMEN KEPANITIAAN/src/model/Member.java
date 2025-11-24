9617cb9e058b252d47cee64c053a2986e290b365
sayedzakiaqram <sayedzakiaqram@gmail.com>
2025-11-24 11:11:05 +0700

    Update Member.java

--- SISTEM MANAJEMEN KEPANITIAAN/src/model/Member.java
+++ SISTEM MANAJEMEN KEPANITIAAN/src/model/Member.java
@@ -1,1 +1,10 @@
+package model;
 
+/**
+ * Member account (terkait ke Anggota via idAnggota).
+ */
+public class Member extends Account {
+    private String idAnggota;
+    public Member(String username, String password, String name, String idAnggota) {
+        super(username,password,name); this.idAnggota = idAnggota;
+    }
