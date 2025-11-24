06df3a4b2429ca2cd413c2f2b5a4d2208febb793
arshaalifamahmud <arshaalifamahmud@gmail.com>
2025-11-24 11:07:53 +0700

    Update Divisi.java

--- SISTEM MANAJEMEN KEPANITIAAN/src/model/Divisi.java
+++ SISTEM MANAJEMEN KEPANITIAAN/src/model/Divisi.java
@@ -1,1 +1,23 @@
+package model;
 
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Kelas Divisi merepresentasikan sebuah divisi dalam organisasi.
+ * Setiap divisi memiliki nama dan daftar tugas yang terkait.
+ *
+ * <p>Objek Divisi biasanya digunakan untuk mengelompokkan tugas-tugas
+ * tertentu berdasarkan bidang atau departemen, misalnya divisi "Acara",
+ * "Humas", atau "Perlengkapan".</p>
+ */
+public class Divisi {
+    private String nama;
+    private final List<Tugas> tugasList = new ArrayList<>();
+
+    /**
+     * Membuat objek Divisi baru dengan nama yang diberikan.
+     *
+     * @param nama nama divisi
+     */
+    public Divisi(String nama) { this.nama = nama; }
