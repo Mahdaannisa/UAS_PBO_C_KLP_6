65a2c704cf3a072d3b73f1fa3ea1d87164d0008f
SilviaPutri <silviaptrrii@gmail.com>
2025-11-24 11:12:29 +0700

    Update Admin.java

--- SISTEM MANAJEMEN KEPANITIAAN/src/model/Admin.java
+++ SISTEM MANAJEMEN KEPANITIAAN/src/model/Admin.java
@@ -1,1 +1,9 @@
+package model;
 
+/**
+ * Admin account.
+ */
+public class Admin extends Account {
+    public Admin(String username, String password, String name) { super(username,password,name); }
+    @Override public String getRole() { return "ADMIN"; }
+}
