f14613fcdc9e09d00bcf683661486694740d9643
zen <zannazikraana@gmail.com>
2025-11-24 14:09:15 +0700

    Update Anggota.java

--- SISTEM MANAJEMEN KEPANITIAAN/src/model/Anggota.java
+++ SISTEM MANAJEMEN KEPANITIAAN/src/model/Anggota.java
@@ -1,1 +1,22 @@
+package model;
 
+/**
+ * Representasi anggota kepanitiaan.
+ * Menyimpan informasi dasar seperti ID, nama, divisi, dan nomor HP.
+ * Class ini digunakan baik oleh admin maupun member untuk proses
+ * pengelolaan data kepanitiaan.
+ */
+public class Anggota {
+    private String id;
+    private String nama;
+    private String divisi;
+    private String noHp;
+
+    /**
+     * Konstruktor untuk membuat objek Anggota baru.
+     *
+     * @param id      ID unik anggota
+     * @param nama    Nama lengkap anggota
+     * @param divisi  Divisi tempat anggota bertugas
+     * @param noHp    Nomor HP anggota
+     */
