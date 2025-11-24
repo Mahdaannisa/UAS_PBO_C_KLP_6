f3509aaa9dc1ee740f46d76d2e3b9feaa5a12a94
SilviaPutri <silviaptrrii@gmail.com>
2025-11-24 21:39:24 +0700

    Implement main entry point for the application
    Added main class to start the application.

--- SISTEM MANAJEMEN KEPANITIAAN/src/app/Main.java
+++ SISTEM MANAJEMEN KEPANITIAAN/src/app/Main.java
@@ -1,1 +1,14 @@
+package app;
+
+import cli.LoginCLI;
+
+/**
+ * Entry point aplikasi Sistem Manajemen Kepanitiaan.
+ */
+public class Main {
+    public static void main(String[] args) {
+        LoginCLI login = new LoginCLI();
+        login.start();
+    }
+}
 
